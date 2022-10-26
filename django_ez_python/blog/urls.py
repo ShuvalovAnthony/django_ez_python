415b7bb4b8b0e12bcc07e2692dbbd0c6e8a7aa4b
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-10-26 19:06:56 +0400

    listview using

--- django_ez_python/blog/urls.py
+++ django_ez_python/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import get_all_topics, get_topics_by_category, blog_home
+from .views import TopicListView, get_topics_by_category, blog_home
 
 # Blog URLS
 urlpatterns = [
     path('', blog_home, name='blog_home'),
-    path('all_topics', get_all_topics, name='all_topics'),
+    path('all_topics/', TopicListView.as_view(), name='all_topics'),
     path('topics_by_category/<str:category>', get_topics_by_category,
      name='topics_by_category')
 ]
--- django_ez_python/blog/views.py
+++ django_ez_python/blog/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render
-from .models import Author, Category, Topic
+from .models import Author, Category, Topic, Genre
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView
+
 
 
 def blog_home(request):
     return render(request, 'blog/blog_home.html')
 
-def get_all_topics(request):
-    all_topics = Topic.objects.all()
-    allowed_viewer = User.objects.get(pk=1)
-    greetings = [
-        'hello',
-        'welcome',
-        'salam aleikum',
-    ]
-    simple_html_el = "<h1>Zagolovok</h1>"
 
-    return render(request, 'blog/all_topics.html', {
-        'all_topics': all_topics,
-        'allowed_viewer': allowed_viewer,
-        'greetings': greetings,
-        'aboba': 'ABOBA palindrom is ABOBA',
-        'simple_html_el': simple_html_el,
-        })
+class TopicListView(ListView):
+    model = Topic
+    template_name = "blog/all_topics.html"
+    context_object_name = "all_topics"
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['allowed_viewer'] = User.objects.get(pk=1)
+        data['all_genres'] = Genre.objects.all()
+        data['greetings'] = 'Hello user!'
+        return data
+
+# def get_all_topics(request):
+#     all_topics = Topic.objects.all()
+#     allowed_viewer = User.objects.get(pk=1)
+#     greetings = [
+#         'hello',
+#         'welcome',
+#         'salam aleikum',
+#     ]
+#     simple_html_el = "<h1>Zagolovok</h1>"
+
+#     return render(request, 'blog/all_topics.html', {
+#         'all_topics': all_topics,
+#         'allowed_viewer': allowed_viewer,
+#         'greetings': greetings,
+#         'aboba': 'ABOBA palindrom is ABOBA',
+#         'simple_html_el': simple_html_el,
+#         })
 
 
 def get_topics_by_category(request, category):
